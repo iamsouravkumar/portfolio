4cc613faca0d1b71fa35493a3a66c9020656b142
sourav <kumarsourav7012@gmail.com>
2024-07-15 18:17:26 +0530

    portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/" style={{ color: "yellow" }}>Portfolio.</Link>
+          <Link className="navbar-brand" to="http://iamsorav.netlify.app" style={{ color: "yellow" }}>Portfolio.</Link>
           <button style={{ backgroundColor: "white" }} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
