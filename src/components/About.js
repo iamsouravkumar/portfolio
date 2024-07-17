bbbdca7190c321dc5923964eca9cbbde0d139f78
sourav <kumarsourav7012@gmail.com>
2024-07-17 16:04:55 +0530

    portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <h1 style={{color:"white", textAlign:"center",fontFamily: "calibri"}} className='about-h1'>About Me</h1>
       <div className='container about'>
         <div className="about-img"><img src="../images/sourav-p.png" alt="Sourav" /></div>
-        <div className='about-text'><p>Hey, I am <span style={{color:"#01E3F4"}}>Sourav</span> from <span style={{color:"#01E3F4"}}>Ghaziabad, UttarPradesh.</span> I am a dedicated <span style={{color:"#01E3F4"}}>Full Stack Developer</span> with expertise in building dynamic, responsive web applications. With a strong foundation in both front-end and back-end technologies, I thrive in creating seamless user experiences and efficient, scalable solutions. My toolkit includes HTML, CSS, JavaScript, React, Node.js, and various other frameworks and libraries. Passionate about continuous learning and staying current with industry trends, I am committed to delivering high-quality code and innovative projects.</p></div>
+        <div className='about-text'><p>Hey, I am <span style={{color:"#01E3F4"}}>Sourav</span> from <span style={{color:"#01E3F4"}}>Ghaziabad, UttarPradesh.</span> I am a dedicated <span style={{color:"#01E3F4"}}>Full Stack Developer</span> with expertise in building dynamic, responsive web applications. With a strong foundation in both front-end and back-end technologies, I thrive in creating seamless user experiences and efficient, scalable solutions. </p><p>My toolkit includes HTML, CSS, JavaScript, React, Node.js, and various other frameworks and libraries. Passionate about continuous learning and staying current with industry trends, I am committed to delivering high-quality code and innovative projects.</p></div>
       </div>
       </div>
     </>
