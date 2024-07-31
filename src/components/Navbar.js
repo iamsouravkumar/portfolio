2d6863e095fda0bd53ad5af44e28383f356f9260
kumarsourav7012 <kumarsourav7012@gmail.com>
2024-07-31 13:01:27 +0000

    portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,7 +50,7 @@ const Navbar = () => {
     <div>
       <nav className={`navbar ${isScrolled ? 'scrolled' : ''} ${stick ? 'stick' : ''}  navbar-expand-lg bg-`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/portfolio" style={{ color: "yellow" }}>Sourav.</Link>
+          <Link className="navbar-brand" to="http://iamsourav.netlify.app" style={{ color: "yellow" }}>Sourav.</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isOpen} aria-label="Toggle navigation" onClick={toggleMenu}>
             <span className="navbar-toggler-icon"></span>
           </button>
