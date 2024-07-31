36fc3f2421e30a7273d200cdd0af7a0d76086d2b
kumarsourav7012 <kumarsourav7012@gmail.com>
2024-07-31 12:32:00 +0000

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import About from './components/About';
 import Footer from './components/Footer';
@@ -12,39 +13,59 @@ import { ToastContainer } from 'react-toastify';
 
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <><Navbar/><Main /></>,
-    },
-    {
-      path: "/home",
-      element: <><Navbar/><Main /></>,
-    },
-    {
-      path: "/portfolio",
-      element: <><Navbar/><Portfolio/></>
-    },
-    {
-      path: "/skill",
-      element: <><Navbar/><SkillSummary /></>,
-    },
-    {
-      path: "/about",
-      element: <><Navbar/><About /></>,
-    },
-    {
-      path: "/contact",
-      element: <><Navbar/><Contact /></>,
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <><Navbar/><Main /></>,
+  //   },
+  //   {
+  //     path: "/home",
+  //     element: <><Navbar/><Main /></>,
+  //   },
+  //   {
+  //     path: "/portfolio",
+  //     element: <><Navbar/><Portfolio/></>
+  //   },
+  //   {
+  //     path: "/skill",
+  //     element: <><Navbar/><SkillSummary /></>,
+  //   },
+  //   {
+  //     path: "/about",
+  //     element: <><Navbar/><About /></>,
+  //   },
+  //   {
+  //     path: "/contact",
+  //     element: <><Navbar/><Contact /></>,
+  //   },
+  // ]);
 
   return (
     <>
-      <RouterProvider router={router} />
+    <Router basename='/portfolio'>
+      <div>
+        <Navbar/>
+        <Main/>
+        <Portfolio/>
+        <About/>
+        <Contact/>
+      <Routes>
+      <Route path="/portfolio" element={<Main />} />
+        <Route path="/home" element={<Main />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/skill" element={<SkillSummary />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <ToastContainer/>
       <ChatButton/>
       <Footer/>
+      </div>
+    </Router>
+      {/* <RouterProvider router={router} />
+      <ToastContainer/>
+      <ChatButton/>
+      <Footer/> */}
     </>
   );
 }
